60aecda9f9d9a0fa2be5d13daea9770a1361eb64
Lukas <85354311+Luk38@users.noreply.github.com>
2025-06-28 22:41:25 +0200

    added constants for scaling + renamed files

--- workspace/robot_control/osc_pose.py
+++ workspace/robot_control/osc_pose.py
@@ -2,50 +2,69 @@ import numpy as np
 import robosuite as suite
 import time
 from receiveHandPositions import receive_hand_positions
-    
-# create environment instance
-env = suite.make(
-    env_name="Lift", 
-    robots="Panda",  
-    has_renderer=True,
-    ignore_done=True,
-    control_freq=60,
-    has_offscreen_renderer=False,
-    use_camera_obs=False,
-)
-env.reset()
 
-obs = env.reset()
+# Constants
+# Scales for the robot's end-effector
+X_POS_SCALE = 0.01  # Scale for robots x position
+Y_POS_SCALE = 0.01  # Scale for robots y position
+Z_POS_SCALE = 0.015  # Scale for robots z position
+X_ROT_SCALE = 10.0  # Scale for robots x rotation
+Y_ROT_SCALE = 10.0  # Scale for robots y rotation   
+Z_ROT_SCALE = 10.0  # Scale for robots z rotation
 
+# Simulation parameters
 MAX_FR = 60 # Maximum frame rate
-while True:
-    start = time.time()
+CONTROL_FREQ = 60  # Control frequency in Hz
 
-    hand_data = receive_hand_positions()
+def main():
+    # create environment instance
+    env = suite.make(
+        env_name="Lift", 
+        robots="Panda",  
+        has_renderer=True,
+        ignore_done=True,
+        control_freq=CONTROL_FREQ,
+        has_offscreen_renderer=False,
+        use_camera_obs=False,
+    )
+    obs = env.reset()
 
-    # calculate delta position
-    current_pos = obs["robot0_eef_pos"]
-    target_pos = np.array([hand_data['z']/100, hand_data['x']/100, hand_data['y']/150])
-    delta_pos = target_pos - current_pos
+    while True:
+        start = time.time()
 
-    # calculate delta rotation
-    # hand_ori = hand_data['orientation']
-    # current_rot = obs["robot0_eef_quat"]
-    # target_rot = np.array([hand_ori['z']/10, hand_ori['x']/10, hand_ori['y']/10])
-    # delta_rot = target_rot - current_rot[:3]
+        # Hand tracking data
+        hand_data = receive_hand_positions()
 
-    # gripper
-    gripper = np.array([hand_data['pinch_strength']])
-    if gripper == 0:
-        gripper = np.array([-1.0])
+        # calculate delta position
+        current_pos = obs["robot0_eef_pos"]
+        target_pos = np.array([hand_data['z']*X_POS_SCALE,
+                            hand_data['x']*Y_POS_SCALE,
+                            hand_data['y']*Z_POS_SCALE])
+        delta_pos = target_pos - current_pos
 
-    # Action: [dx, dy, dz, droll, dpitch, dyaw, gripper]
-    action = np.concatenate([delta_pos, np.zeros(3), gripper])
+        # calculate delta rotation
+        hand_ori = hand_data['orientation']
+        current_rot = obs["robot0_eef_quat"]
+        target_rot = np.array([hand_ori['z']*X_ROT_SCALE,
+                               hand_ori['x']*Y_ROT_SCALE,
+                               hand_ori['y']*Z_ROT_SCALE,])
+        delta_rot = target_rot - current_rot[:3]
 
-    obs, reward, done, info = env.step(action)
-    env.render()
+        # gripper
+        gripper = np.array([hand_data['pinch_strength']])
+        if gripper == 0:
+            gripper = np.array([-1.0])
 
-    elapsed = time.time() - start
-    diff = 1 / MAX_FR - elapsed
-    if diff > 0:
-        time.sleep(diff)
+        # Action: [dx, dy, dz, droll, dpitch, dyaw, gripper]
+        action = np.concatenate([delta_pos, np.zeros(3), gripper])
+
+        obs, reward, done, info = env.step(action)
+        env.render()
+
+        elapsed = time.time() - start
+        diff = 1 / MAX_FR - elapsed
+        if diff > 0:
+            time.sleep(diff)
+
+if __name__ == "__main__":
+    main()
---
+++ workspace/robot_control/receive_hand_positions.py
@@ -0,0 +1,27 @@
+import socket
+import json
+
+#Default values for the IP, PORT, and BUFFER_SIZE
+IP  = "127.0.0.1" #localhost
+PORT = 5005
+BUFFER_SIZE = 1024
+
+def receive_hand_positions(ip=IP, port=PORT, buffer_size=BUFFER_SIZE):
+    """
+    Generator function to receive hand positions from a UDP socket.
+    
+    Args:
+        ip (str): IP address to bind the socket to.
+        port (int): Port number to bind the socket to.
+        buffer_size (int): Size of the buffer for receiving data.
+        
+    returns:
+        dict: Parsed hand data received from the socket.
+    """
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port))
+
+    while True:
+        data, addr = sock.recvfrom(buffer_size)
+        hand_data = json.loads(data.decode())
+        return hand_data
